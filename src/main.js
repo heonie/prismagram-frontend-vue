71874b5f5934cc5661b9816d7e55e32cd384afaf
Kookheon Kim <kookheon.kim@samsung.com>
2020-07-01 19:22:49 +0900

    Initial Vue Apollo template using apollo-boost and vue-apollo

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueApollo from 'vue-apollo'
+import apolloClient from "./apolloClient";
 
 Vue.config.productionTip = false
+
+Vue.use(VueApollo)
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
