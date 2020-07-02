e6e592f9ad01bdcf9f32f20281bebd8508f6d8f0
Kookheon Kim <kookheon.kim@samsung.com>
2020-07-02 15:03:05 +0900

    Theming with SCSS

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,82 +1,74 @@
 <template>
-    <FooterContainer>
-        <List>
-            <ListItem>
-                <LinkTag href="#">about us</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">support</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">press</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">api</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">jobs</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">privacy</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">terms</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">directory</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">profiles</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">hashtags</LinkTag>
-            </ListItem>
-            <ListItem>
-                <LinkTag href="#">language</LinkTag>
-            </ListItem>
-        </List>
-        <Copyright>Instaclone {{new Date().getFullYear()}} &copy;</Copyright>
-    </FooterContainer>
+    <footer>
+        <ul>
+            <li>
+                <a href="#">about us</a>
+            </li>
+            <li>
+                <a href="#">support</a>
+            </li>
+            <li>
+                <a href="#">press</a>
+            </li>
+            <li>
+                <a href="#">api</a>
+            </li>
+            <li>
+                <a href="#">jobs</a>
+            </li>
+            <li>
+                <a href="#">privacy</a>
+            </li>
+            <li>
+                <a href="#">terms</a>
+            </li>
+            <li>
+                <a href="#">directory</a>
+            </li>
+            <li>
+                <a href="#">profiles</a>
+            </li>
+            <li>
+                <a href="#">hashtags</a>
+            </li>
+            <li>
+                <a href="#">language</a>
+            </li>
+        </ul>
+        <span>Instaclone {{new Date().getFullYear()}} &copy;</span>
+    </footer>
 </template>
 
 <script>
-import styled from 'vue-styled-components';
-
-const FooterContainer = styled.footer`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  text-transform: uppercase;
-  font-weight: 600;
-  font-size: 12px;
-  margin: 50px 0px;;
-`;
-const List = styled.ul`
-  display: flex;
-`;
-
-const ListItem = styled.li`
-  &:not(:last-child) {
-    margin-right: 16px;
-  }
-`;
-
-const LinkTag = styled.a`
-  color: ${props => props.theme.darkBlueColor};
-`;
-
-const Copyright = styled.span`
-  color: ${props => props.theme.darkGreyColor};
-`;
-
 export default {
-    name: "Footer",
-    components: {
-        FooterContainer, List, ListItem, LinkTag, Copyright
-    }
+    name: "Footer"
 }
 </script>
 
 <style scoped lang="scss">
+@import '../styles/themes';
+@use "sass:map";
+footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 12px;
+    margin: 50px 0px;
+}
 
+ul {
+    display: flex;
+}
+li:not(:last-child) {
+    margin-right: 16px;
+}
+
+a {
+    color: map-get($themes, "darkBlueColor");
+}
+span {
+    color: map-get($themes, "darkGreyColor");
+}
 </style>
